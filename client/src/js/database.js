2ce7e14825ddf0827babbcf271ebbb84cd50b5b4
ssimmons122 <121777930+ssimmons122@users.noreply.github.com>
2023-06-11 22:18:51 -0400

    add.get data from db

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,24 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// Add text to indexDB
+export const putDb = async (content) => {
+      const jateDB = await openDB('jate', 1);
+      const tx = jateDB.transaction('jate', 'readwrite');
+      const store = tx.objectStore('jate');
+      const request = store.put({ id: 1, value: content });
+      const result = await request;
+      console.log("Data saved to database", result);
+};
+// Get data from indexdb
+export const getDb = async () => {
+    	const jateDb = await openDB('jate', 1);
+	    const tx = jateDb.transaction('jate', 'readonly');
+	    const store = tx.objectStore('jate');
+	    const request = store.getAll();
+	    const result = await request;
+	    console.log(result);
+};
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
 
 initdb();
